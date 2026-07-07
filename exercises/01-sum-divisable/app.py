14c8870888179705989e783dea7c96ed5f998775
José Isidro <isidro107109@gmail.com>
2026-07-07 12:16:18 +0000

    ejercicios de python

--- exercises/01-sum-divisable/app.py
+++ exercises/01-sum-divisable/app.py
@@ -2,6 +2,8 @@ sum_result = 0
 
 # TODO: Write your solution here.
 # Sum all numbers below 1000 that are divisible by 3 or 5.
-
+for num in range(1000):
+    if num % 3 == 0 or num % 5 == 0:
+        sum_result += num
 # Use print to show the result
 print(sum_result)
--- exercises/02-print-tic-toc/app.py
+++ exercises/02-print-tic-toc/app.py
@@ -1,3 +1,12 @@
 # TODO: Write your solution here.
 # Print numbers from 1 to 100.
 # Multiples of 3 => "Tic", multiples of 5 => "Toc", multiples of both => "TicToc".
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("TicToc")
+    elif num % 3 == 0:
+        print("Tic")
+    elif num % 5 == 0:
+        print("toc")
+    else:
+        print(num)
